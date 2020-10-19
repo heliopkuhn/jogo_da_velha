16587f52598a03b80b191b2f24d220db884a365f
heliopkuhn <heliopkuhn@gmail.com>
2020-10-18 21:42:22 -0300

    V-1.0

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ class Game{
         for(let i=0;i<elementos.length; i++){
 
             elementos[i].addEventListener("click",function pasarajogada(){
-                if(!elementos[i].children[0].classList.contains("circulo", "xis")){
+                if(elementos[i].children[0].className !="circulo" && elementos[i].children[0].className!="xis"){
                     if(jogadas%2==0){
                         
                         elementos[i].children[0].classList.add("circulo");
@@ -56,15 +56,58 @@ class Game{
 
                 function vencedor_01(el1,el2,el3){
                     if(el1.children[0].className ==  "circulo" && el2.children[0].className =="circulo" && el3.children[0].classList =="circulo"){
-                        alert("Ganhou!!!");
+                        let mostrarvencedor = document.getElementsByClassName("hiden");
+                        mostrarvencedor[0].innerHTML = "Bolinha Ganhou!!!"
+                        mostrarvencedor[0].style.display = "block";
+                        let botao = document.getElementsByClassName("btn");
+                        botao[0].style.display = "inline-block";
+                    }
+                }
+
+                function vencedor_02(val1, val2, val3){
+                    if(val1.children[0].className ==  "xis" && val2.children[0].className =="xis" && val3.children[0].classList =="xis"){
+                        let mostrarvencedor = document.getElementsByClassName("hiden");
+                        mostrarvencedor[0].innerHTML = "Xis venceu!!!";
+                        mostrarvencedor[0].style.display = "block";
+                        let botao = document.getElementsByClassName("btn");
+                        botao[0].style.display = "inline-block";
                     }
                 }
 
                 vencedor_01(validarJogada()[0], validarJogada()[1], validarJogada()[2]);
                 vencedor_01(validarJogada()[0], validarJogada()[4], validarJogada()[8]);
-            });
-        }
+                vencedor_01(validarJogada()[0], validarJogada()[3], validarJogada()[6]);
+                vencedor_01(validarJogada()[1], validarJogada()[4], validarJogada()[7]);
+                vencedor_01(validarJogada()[2], validarJogada()[4], validarJogada()[6]);
+                vencedor_01(validarJogada()[2], validarJogada()[5], validarJogada()[8]);
+                vencedor_01(validarJogada()[3], validarJogada()[4], validarJogada()[5]);
+                vencedor_01(validarJogada()[6], validarJogada()[7], validarJogada()[8]);
 
+                vencedor_02(validarJogada()[0], validarJogada()[1], validarJogada()[2]);
+                vencedor_02(validarJogada()[0], validarJogada()[4], validarJogada()[8]);
+                vencedor_02(validarJogada()[0], validarJogada()[3], validarJogada()[6]);
+                vencedor_02(validarJogada()[1], validarJogada()[4], validarJogada()[7]);
+                vencedor_02(validarJogada()[2], validarJogada()[4], validarJogada()[6]);
+                vencedor_02(validarJogada()[2], validarJogada()[5], validarJogada()[8]);
+                vencedor_02(validarJogada()[3], validarJogada()[4], validarJogada()[5]);
+                vencedor_02(validarJogada()[6], validarJogada()[7], validarJogada()[8]);
+
+            });
+
+        }
+    }
+
+    novoJogo(){
+        let elementos = document.getElementsByClassName("tabela");
+        for (let i=0;i<elementos.length; i++){
+            elementos[i].children[0].className = "";
+            elementos[i].children[0].innerHTML = "";
+        }
+        jogadas = 0;
+        let painelVencedor = document.getElementsByClassName("hiden");
+        painelVencedor[0].style.display = "none";
+        let btn = document.getElementsByClassName("btn");
+        btn[0].style.display = "none";
     }
 
 }
